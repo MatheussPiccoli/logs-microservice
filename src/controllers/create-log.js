97b61fbc7cd7387ce89e3220e856f6fa358c0108
Matheus Piccoli <matheuspdmoura@gmail.com>
2026-06-07 23:57:48 -0300

    feat: add CreateLogController

---
+++ src/controllers/create-log.js
@@ -0,0 +1,55 @@
+import {
+  badRequest,
+  created,
+  serverError,
+  checkIfActionIsValid,
+} from "../helpers/index.js";
+
+export class CreateLogController {
+  constructor(createLogUseCase) {
+    this.createLogUseCase = createLogUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const { delivery, email, locker_id, password } = httpRequest.body;
+
+      if (!delivery) {
+        return badRequest({
+          message: "delivery is required",
+        });
+      }
+
+      if (!email) {
+        return badRequest({
+          message: "email is required",
+        });
+      }
+
+      if (!locker_id) {
+        return badRequest({
+          message: "locker_id is required",
+        });
+      }
+
+      if (!checkIfActionIsValid(action)) {
+        return badRequest({
+          message: "Invalid action",
+        });
+      }
+
+      const log = await this.createLogUseCase.execute({
+        delivery,
+        email,
+        locker_id,
+        password,
+      });
+
+      return created(log);
+    } catch (error) {
+      console.error(error);
+
+      return serverError();
+    }
+  }
+}
